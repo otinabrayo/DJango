779ff689c0fcb5e392e2aa97396d1b70b85efed9
brian <brianotina20@gmail.com>
2024-08-15 14:46:53 +0300

    newFnctionalities

--- Django_Project/Django_Project/urls.py
+++ Django_Project/Django_Project/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 from django.views.generic import TemplateView
 from users import views as user_views
 
@@ -28,3 +30,6 @@ urlpatterns = [
     path('logout/',user_views.logout_view,name='logout'),
     path('', include("vlog.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
